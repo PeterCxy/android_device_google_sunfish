ccf9ba4eff68d6b3a852836225fd0fe928e99b4f
Peter Cai <peter@typeblog.net>
2020-12-29 10:53:35 +0800

    Revert "sunfish: Exclude lineage-sdk overlays from RRO"
    This reverts commit 392e8376c32a1e8669dadb817eb74b274844f3d7.

--- device-lineage.mk
+++ device-lineage.mk
@@ -32,8 +32,6 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
 
 # EUICC
 PRODUCT_COPY_FILES += \
