4700ffc46bf8539e1cf14880fc7e6c746afad6a3
Peter Cai <peter@typeblog.net>
2020-12-29 10:54:39 +0800

    Revert "sunfish: Dexpreopt speed compile SystemUI"
    This reverts commit 5db64eb4608d7d34d7be08e98ffdf9cf797ce117.
    
    * Done globally in vendor/lineage

--- device.mk
+++ device.mk
@@ -760,7 +760,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SystemUIGoogle
 
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := true
